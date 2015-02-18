4b374d3be1fbfe45ea4574ded15554d81a5bcb51
lester-s <simon.le-ster@soat.fr>
2015-02-18 17:04:00 +0100

    test3

--- ViewModelBase/ViewModelBase.cs
+++ ViewModelBase/ViewModelBase.cs
@@ -8,7 +8,6 @@ namespace ViewModelBase
 {
     public class ViewModelBase:INotifyPropertyChanged
     {
-        //hello
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void NotifyOfPropertyChanged<TProperty>(Expression<Func<TProperty>> property)
